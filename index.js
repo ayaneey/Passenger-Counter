d620f62b213ab50bfbd6ba620966629a45f3ec8c
Ayan Ahmed <ayanahmed@ayans-mbp.home>
2023-01-03 11:48:27 +0000

    made changes to save button

--- index.js
+++ index.js
@@ -26,7 +26,14 @@ decrement();
 
 // creating save button
 
+let saveEl = document.getElementById("previous-entries");
+
 function save() {
+	let countStr = count + " - ";
+	// 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+	// 3. Render the variable in the saveEl using innerText
+	saveEl.textContent += countStr;
+	// NB: Make sure to not delete the existing content of the paragraph
 	console.log(count);
 }
 
